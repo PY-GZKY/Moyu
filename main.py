7fbe41e62dd29e5d3b263fd0a6510045535ac1e8
PY-GZKY <341796767@qq.com>
2021-12-30 09:13:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,42 +14,6 @@ app = FastAPI(
 
 templates = Jinja2Templates(directory="templates")
 
-today = datetime.date.today()
-
-# print(today.year, today.month, today.day)
-# print("大年时间: ", lunar_date(today.year+1, 1, 1).to_datetime().date())
-# print("端午时间: ", lunar_date(today.year, 5, 5).to_datetime().date())
-# print("中秋时间: ", lunar_date(today.year, 8, 15).to_datetime().date())
-# print("元旦时间: ", f"{today.year+1}-01-01")
-# print("清明时间: ", f"{today.year+1}-04-05")
-# print("劳动时间: ", f"{today.year+1}-05-01")
-# print("国庆时间: ", f"{today.year+1}-10-01")
-
-distance_big_year = (lunar_date(today.year + 1, 1, 1).to_datetime().date() - today).days
-
-distance_5_5 = (lunar_date(today.year, 5, 5).to_datetime().date() - today).days
-distance_5_5 = distance_5_5 if distance_5_5 > 0 else (
-        lunar_date(today.year + 1, 5, 5).to_datetime().date() - today).days
-
-distance_8_15 = (lunar_date(today.year, 8, 15).to_datetime().date() - today).days
-distance_8_15 = distance_8_15 if distance_8_15 > 0 else (
-        lunar_date(today.year + 1, 8, 15).to_datetime().date() - today).days
-
-distance_year = (datetime.datetime.strptime(f"{today.year + 1}-01-01", "%Y-%m-%d").date() - today).days
-
-distance_4_5 = (datetime.datetime.strptime(f"{today.year}-04-05", "%Y-%m-%d").date() - today).days
-distance_4_5 = distance_4_5 if distance_4_5 > 0 else (
-        datetime.datetime.strptime(f"{today.year + 1}-04-05", "%Y-%m-%d").date() - today).days
-
-distance_5_1 = (datetime.datetime.strptime(f"{today.year}-05-01", "%Y-%m-%d").date() - today).days
-distance_5_1 = distance_5_1 if distance_5_1 > 0 else (
-        datetime.datetime.strptime(f"{today.year + 1}-05-01", "%Y-%m-%d").date() - today).days
-
-distance_10_1 = (datetime.datetime.strptime(f"{today.year}-10-01", "%Y-%m-%d").date() - today).days
-distance_10_1 = distance_10_1 if distance_10_1 > 0 else (
-        datetime.datetime.strptime(f"{today.year + 1}-10-01", "%Y-%m-%d").date() - today).days
-
-
 def get_week_day(date):
     week_day_dict = {
         0: '星期一',
@@ -63,34 +27,70 @@ def get_week_day(date):
     day = date.weekday()
     return week_day_dict[day]
 
+def time_parse(today):
+    # print(today.year, today.month, today.day)
+    # print("大年时间: ", lunar_date(today.year+1, 1, 1).to_datetime().date())
+    # print("端午时间: ", lunar_date(today.year, 5, 5).to_datetime().date())
+    # print("中秋时间: ", lunar_date(today.year, 8, 15).to_datetime().date())
+    # print("元旦时间: ", f"{today.year+1}-01-01")
+    # print("清明时间: ", f"{today.year+1}-04-05")
+    # print("劳动时间: ", f"{today.year+1}-05-01")
+    # print("国庆时间: ", f"{today.year+1}-10-01")
 
-# print("距离大年: ", distance_big_year)
-# print("距离端午: ", distance_5_5)
-# print("距离中秋: ", distance_8_15)
-# print("距离元旦: ", distance_year)
-# print("距离清明: ", distance_4_5)
-# print("距离劳动: ", distance_5_1)
-# print("距离国庆: ", distance_10_1)
-# print("距离周末: ", 5 - today.weekday())
+    distance_big_year = (lunar_date(today.year + 1, 1, 1).to_datetime().date() - today).days
 
-now_ = f"{today.year}年{today.month}月{today.day}日"
-week_day_ = get_week_day(today)
-time_ = [
-    {"v_": 5 - 1 - today.weekday(), "title": "周末"},  # 距离周末
-    {"v_": distance_year, "title": "元旦"},  # 距离元旦
-    {"v_": distance_big_year, "title": "过年"},  # 距离过年
-    {"v_": distance_4_5, "title": "清明节"},  # 距离清明
-    {"v_": distance_5_1, "title": "劳动节"},  # 距离劳动
-    {"v_": distance_5_5, "title": "端午节"},  # 距离端午
-    {"v_": distance_8_15, "title": "中秋节"},  # 距离中秋
-    {"v_": distance_10_1, "title": "国庆节"},  # 距离国庆
-]
+    distance_5_5 = (lunar_date(today.year, 5, 5).to_datetime().date() - today).days
+    distance_5_5 = distance_5_5 if distance_5_5 > 0 else (
+            lunar_date(today.year + 1, 5, 5).to_datetime().date() - today).days
 
-time_ = sorted(time_, key=lambda x: x['v_'], reverse=False)
+    distance_8_15 = (lunar_date(today.year, 8, 15).to_datetime().date() - today).days
+    distance_8_15 = distance_8_15 if distance_8_15 > 0 else (
+            lunar_date(today.year + 1, 8, 15).to_datetime().date() - today).days
+
+    distance_year = (datetime.datetime.strptime(f"{today.year + 1}-01-01", "%Y-%m-%d").date() - today).days
+
+    distance_4_5 = (datetime.datetime.strptime(f"{today.year}-04-05", "%Y-%m-%d").date() - today).days
+    distance_4_5 = distance_4_5 if distance_4_5 > 0 else (
+            datetime.datetime.strptime(f"{today.year + 1}-04-05", "%Y-%m-%d").date() - today).days
+
+    distance_5_1 = (datetime.datetime.strptime(f"{today.year}-05-01", "%Y-%m-%d").date() - today).days
+    distance_5_1 = distance_5_1 if distance_5_1 > 0 else (
+            datetime.datetime.strptime(f"{today.year + 1}-05-01", "%Y-%m-%d").date() - today).days
+
+    distance_10_1 = (datetime.datetime.strptime(f"{today.year}-10-01", "%Y-%m-%d").date() - today).days
+    distance_10_1 = distance_10_1 if distance_10_1 > 0 else (
+            datetime.datetime.strptime(f"{today.year + 1}-10-01", "%Y-%m-%d").date() - today).days
+
+    # print("距离大年: ", distance_big_year)
+    # print("距离端午: ", distance_5_5)
+    # print("距离中秋: ", distance_8_15)
+    # print("距离元旦: ", distance_year)
+    # print("距离清明: ", distance_4_5)
+    # print("距离劳动: ", distance_5_1)
+    # print("距离国庆: ", distance_10_1)
+    # print("距离周末: ", 5 - today.weekday())
+
+    time_ = [
+        {"v_": 5 - today.weekday(), "title": "周末"},  # 距离周末
+        {"v_": distance_year, "title": "元旦"},  # 距离元旦
+        {"v_": distance_big_year, "title": "过年"},  # 距离过年
+        {"v_": distance_4_5, "title": "清明节"},  # 距离清明
+        {"v_": distance_5_1, "title": "劳动节"},  # 距离劳动
+        {"v_": distance_5_5, "title": "端午节"},  # 距离端午
+        {"v_": distance_8_15, "title": "中秋节"},  # 距离中秋
+        {"v_": distance_10_1, "title": "国庆节"},  # 距离国庆
+    ]
+
+    time_ = sorted(time_, key=lambda x: x['v_'], reverse=False)
+    return time_
 
 
 @app.get("/", response_class=HTMLResponse)
 async def readme(request: Request):
+    today = datetime.date.today()
+    now_ = f"{today.year}年{today.month}月{today.day}日"
+    week_day_ = get_week_day(today)
+    time_ = time_parse(today)
     return templates.TemplateResponse("readme.html",
                                       {"request": request, "time_": time_, "now_": now_, "week_day_": week_day_})
 
